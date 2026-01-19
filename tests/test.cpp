5ebfc3c266a71fe0a0f8b0559c3f0695c7ffa61e
dpkn1989 <dpkn1989@gmail.com>
2026-01-19 10:30:19 +0100

    Test Fixture added to test.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,24 @@
 #include <gtest/gtest.h>
 #include "Car.h"
 
+struct Car_Test : public testing::Test
+{
+   Car c1,c2,c3;
+   
+   Car_Test(): c1("Benz", "Blue", 40, 24.5f), 
+               c2("BMW", "Black", 35, 36.3f), 
+               c3() 
+   {}
+   
+   void SetUp() {}
+   void TearDown() {}
+};
+
+TEST_F(Car_Test, StaticCountTest)
+{
+   ASSERT_EQ(Car::car_count(), 3);
+}
+
 TEST(CAR_CLASS, Constructor_case)
 {
    Car c;
